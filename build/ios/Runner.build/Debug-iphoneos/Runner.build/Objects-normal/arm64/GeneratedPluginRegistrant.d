f1acffea162f6a3eb64e383997801be5eb446506
meshal <qualitymeshal@gmail.com>
2020-12-14 00:19:13 +0300

    cimmit for food waste project

---
+++ build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,18 @@
+dependencies: \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/Flutter.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h
---
+++ build/ios/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/meshal/Desktop/flutter\ objet\ detaction/ObjectDetection_SSD/ssd_yolo_demo/build/ios/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
